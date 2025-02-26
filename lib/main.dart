7146ba63ccbe4e768108c64436335f07dc32c9f6
Frida_Cera <fridaapodaca541@gmail.com>
2025-02-26 20:09:14 +0000

    enviado

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisIconosAPP());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconosAPP extends StatelessWidget {
+  const MisIconosAPP({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("frida apodaca"),
+          titleTextStyle: TextStyle(color: Color(0xffb4c0e1)),
+          centerTitle: true,
+          backgroundColor: Color(0xff1d3857),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(
+              'frida sofia apodaca cera MAT:22308051281136',
+              style: TextStyle(fontSize: 20, color: Color(0xff103d52)),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Icon(
+                  Icons.thumb_down_alt_sharp,
+                  color: Color(0xff820a0a),
+                  size: 24.0,
+                  semanticLabel: 'Text to announce in accessibility modes',
+                ),
+                Icon(
+                  Icons.search,
+                  color: Color(0xff102275),
+                  size: 30.0,
+                ),
+                Icon(
+                  Icons.thumb_up_alt_sharp,
+                  color: Color(0xff3dff36),
+                  size: 36.0,
+                ),
+                Icon(
+                  Icons.settings,
+                  color: Color(0xff919291),
+                  size: 36.0,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
